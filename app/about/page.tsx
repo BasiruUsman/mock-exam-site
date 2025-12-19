be28e480d9bf365bfd0b25b26005564b88d445c2
BasiruUsman <111478047+BasiruUsman@users.noreply.github.com>
2025-12-18 22:11:28 -0500

    Correct capitalization in 'Coming soon' message

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ export default function Exams() {
   return (
     <main className="p-8">
       <h1 className="text-2xl font-semibold">About TsafeLabs</h1>
-      <p className="mt-2">Coming soon: yeh.</p>
+      <p className="mt-2">Coming soon: Yeh.</p>
     </main>
   );
 }
